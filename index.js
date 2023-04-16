3f4f6cf3af97060baf8b1b2afe485ab7f2063ab5
aniketjadhav1812 <128202035+aniketjadhav1812@users.noreply.github.com>
2023-04-16 14:47:33 +0530

    anj
    iu

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 var typed = new Typed('#text1', {
-    strings: ['Coder.', 'Developer.','Student.'],
+    strings: ['Coder.', 'Developer.', 'Student.'],
     typeSpeed: 50,
     backspeed: 40,
     loop: true,
-    loopcount : Infinity,
+    loopcount: Infinity,
     smartBackspace: true,
 });
 
+const closemenu = () => {
+    document.getElementById("side").style.left = "-100%";
+    document.getElementById('menu').style.left = "25px"
+    document.getElementById('slide').style.top = "-100%"    
+
+    
+}
+const openmenu = () => {
+    document.getElementById('menu').style.left = "-100%"
+    document.getElementById("side").style.left = "0%";
+    document.getElementById('slide').style.top = "0%"    
+}
+
+
 var menubtns = document.getElementsByClassName("menu-btn");
 var tabs = document.getElementsByClassName("tabs");
-const opentab = (tabname) =>{
-    
+const opentab = (tabname) => {
+
     for (const menubtn of menubtns) {
-     menubtn.classList.remove('active-btn');   
+        menubtn.classList.remove('active-btn');
     }
     for (const tab of tabs) {
         tab.classList.remove('active');
